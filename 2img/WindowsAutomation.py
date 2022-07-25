8fc12b50d139d85390088b79e3dd9734732fac59
Hyprnx <toducanh2001@gmail.com>
2022-07-25 21:56:03 +0700

    update saving automation
    currently be able to browse

--- 2img/WindowsAutomation.py
+++ 2img/WindowsAutomation.py
@@ -4,15 +4,18 @@ from configparser import ConfigParser
 from base import BaseClass
 
 
+# Currently only support Windows 10 and Office 365 only, if you want to support other OS/office version,
+# please contact me, or if you can do it yourself, create a PR :D
 
 class AutomationGenerator(BaseClass):
     def __init__(self):
         super().__init__()
-        config_path ='common/config.ini'
+        config_path = 'common/config.ini'
         self.log.info(f'Reading config file from {config_path}')
         config = ConfigParser()
         config.read_file(open(config_path))
         application_path = config.get('local', 'application_path')
+        self.save_path = config.get('local', 'save_path')
         self.log.info("Opening Word")
         self.app = application.Application(backend="uia")
         self.app.start(application_path)
@@ -21,18 +24,55 @@ class AutomationGenerator(BaseClass):
         self._open_blank_document()
 
     def _experimental(self):
-        self.app['Document1 - Word']['Document'].Custom.Edit.TypeKeys("Hello World")
+        dev_default_id = '123456'
+        text = "hello world"
+        self.app.Document['File Tab'].click()
+        self.app.Document['Save As'].select()
+        self.app.Document['Browse'].click()
+        # self.app.Document['Browse'].click()
+        # self.app.Document['Address: Documents'].toggle()
+        self.app.Document.print_control_identifiers()
+
 
     def _open_blank_document(self):
         self.log.info("Opening blank document")
         self.app.Word.Pane0.Pane3.HomeGroupBox.HomePane.NewGroupBox.NewListBox.ListItem0.invoke()
         self.log.info("Blank document opened")
 
-    # def edit_document(self, text):
-    #     self.log.info("Editing document")
-    #     self.app['Document1 - Word']['Document']["Custom"]["Edit"].TypeKeys(text)
-    #     self.log.info("Document edited")
+    def _edit_document(self, text):
+        # apparently, if this doesn't work, try to close all Word windows, it should work, if it still not,
+        # create a issue
+        self.log.info("Editing document")
+        text = text.replace('\n', '{ENTER}')
+        text = text.replace('\t', '{TAB}')
+        text = text.replace(' ', '{VK_SPACE}')
+        self.app['Dialog']['Document'].type_keys(text)
+        self.log.info("Document edited")
+
+    def fetch_data(self):
+        pass
+
+    def save_document(self, text):
+        self._edit_document(text)
+        self.log.info("Saving document")
+        self.app['Dialog']['Document'].type_keys('{ENTER}')
+        self.log.info("Document saved")
+
 
 if __name__ == '__main__':
-    text = 'Hello World'
+    text = '(Dân trí) - Ông Trần Văn Dự (SN 1961) - Nguyên Phó Cục trưởng Cục Quản lý xuất nhập cảnh, Bộ Công an, ' \
+           'bị khởi tố, bắt tạm giam để điều tra về tội "Nhận hối lộ". Tối 25/7, Bộ Công an cho biết, mở rộng điều ' \
+           'tra vụ án "Đưa hối lộ, nhận hối lộ" xảy ra tại Cục Lãnh sự Bộ Ngoại giao, Hà Nội và các tỉnh, thành phố, ' \
+           'ngày 25/7, Cơ quan An ninh điều tra Bộ Công an đã ra quyết định bổ sung quyết định khởi tố vụ án hình sự ' \
+           '"Lừa đảo chiếm đoạt tài sản"; quyết định khởi tố bị can, lệnh bắt bị can để tạm giam, lệnh khám xét chỗ ' \
+           'ở, nơi làm việc đối với 6 bị can: Bà Nguyễn Mai Anh (SN 1976, tại Quảng Ninh) - Chuyên viên Vụ Quan hệ ' \
+           'quốc tế Văn phòng Chính phủ;\n Ông Ngô Quang Tuấn (SN 1984, tại Hà Nội) - Chuyên viên Vụ Hợp tác quốc tế Bộ ' \
+           'Giao thông vận tải; Ông Trần Văn Dự (SN 1961, tại Thái Bình) - Nguyên Phó Cục trưởng Cục Quản lý xuất ' \
+           'nhập cảnh Bộ Công an; Ông Vũ Sỹ Cường (SN 1986, tại Hưng Yên) - Nguyên cán bộ Cục Quản lý xuất nhập cảnh ' \
+           'Bộ Công an;Các bị can trên cùng bị khởi tố về tội "Nhận hối lộ", quy định tại Điều 354 Bộ luật Hình sự. ' \
+           '\nNgoài ra, Cơ quan An ninh điều tra Bộ Công an còn khởi tố ông Bùi Huy Hoàng (SN 1988, tại Hải Dương) - ' \
+           'Cán bộ Cục Y tế dự phòng Bộ Y tế, về tội "Lừa đảo chiếm đoạt tài sản" quy định tại Điều 174 Bộ luật Hình ' \
+           'sự; Ông Nguyễn Tiến Mạnh (SN 1970, tại Bắc Giang) - Phó Giám đốc Công ty cổ phần du lịch thương mại Lữ ' \
+           'Hành Việt, Giám đốc Công ty vận tải du lịch Hoàng Long Luxury, bị khởi tố về tội "Đưa hối lộ" quy định ' \
+           'tại Điều 364 Bộ luật Hình sự. '
     AutomationGenerator()._experimental()
